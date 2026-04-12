e73d7c3427f3c7dc1154db69b20edf8ac29ec2fa
Gvtone <geraldtolentino82@yahoo.com.ph>
2026-04-13 01:00:13 +0800

    Created layout for auth page

---
+++ apps/web/src/components/layout/AuthLayout.tsx
@@ -0,0 +1,44 @@
+import { Sparkles } from "lucide-react";
+import { moonSky } from "../../assets";
+
+function AuthLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex min-h-screen">
+      <div
+        className="relative hidden md:flex items-center justify-center 
+        w-1/2 text-white p-12 bg-center bg-cover"
+        style={{ backgroundImage: `url(${moonSky})` }}
+      >
+        <div className="flex flex-col gap-4 justify-center items-center z-1">
+          <div className="flex justify-center items-center bg-linear-to-br from from-hobbly-sky to-hobbly-lavender p-4 size-16 rounded-2xl">
+            <Sparkles></Sparkles>
+          </div>
+          <h1 className="text-5xl">Hobbly</h1>
+          <span className="text-xl mb-4">
+            "A cozy place for everything you love."
+          </span>
+          <div className="flex gap-4 text-3xl">
+            <span>✨</span>
+            <span>🎌</span>
+            <span>📚</span>
+            <span>🎮</span>
+            <span>🎨</span>
+            <span>🍜</span>
+            <span>🎵</span>
+            <span>🌿</span>
+          </div>
+        </div>
+        <div className="absolute bg-linear-to-br from-[#0f1b35d9] to-[#5bb3d04d] size-full"></div>
+      </div>
+
+      <div
+        className="flex flex-col items-center justify-center 
+        w-full md:w-1/2 bg-background p-12"
+      >
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default AuthLayout;
---
+++ apps/web/src/pages/AuthPage.tsx
@@ -0,0 +1,11 @@
+import AuthLayout from "../components/layout/AuthLayout";
+
+function AuthPage() {
+  return (
+    <AuthLayout>
+      <div></div>
+    </AuthLayout>
+  );
+}
+
+export default AuthPage;
