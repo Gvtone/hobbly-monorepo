a51284ee835f26ecf50385406a2b520daa082931
Gvtone <geraldtolentino82@yahoo.com.ph>
2026-04-14 14:52:27 +0800

    Added comments for readability

--- apps/web/src/pages/AuthPage.tsx
+++ apps/web/src/pages/AuthPage.tsx
@@ -27,6 +27,8 @@ function AuthPage() {
             Back to Hobbly
           </a>
         </div>
+
+        {/* Main card */}
         <Card className="items-center p-10 shadow-xl mb-6">
           <div className="flex flex-col items-center gap-2 mb-8">
             <h2 className="text-center text-lg xs:text-2xl">
@@ -38,6 +40,8 @@ function AuthPage() {
                 : "Start your cozy hobby journey"}
             </span>
           </div>
+
+          {/* Switch */}
           <div className="flex flex-col xs:flex-row bg-accent rounded-2xl xs:rounded-full w-full py-1.5 px-2 mb-6">
             {(["login", "signup"] as const).map(m => {
               return (
@@ -54,6 +58,8 @@ function AuthPage() {
               );
             })}
           </div>
+
+          {/* Form */}
           <form onSubmit={handleSubmit} className="flex flex-col w-full mb-8">
             {mode === "signup" && (
               <div className="flex flex-col mb-6">
@@ -104,6 +110,7 @@ function AuthPage() {
               {mode === "login" ? "Log in to Hobbly" : "Create my space"}
             </Button>
           </form>
+
           <div className="w-full flex justify-center items-center mb-8">
             <div className="flex-1 h-px border border-border"></div>
             <span className="text-muted-foreground text-sm rounded-full bg-card px-2">
@@ -111,6 +118,8 @@ function AuthPage() {
             </span>
             <div className="flex-1 h-px border border-border"></div>
           </div>
+
+          {/* Sign in with Google */}
           <Button
             variant="secondary"
             shape="pill"
@@ -141,6 +150,8 @@ function AuthPage() {
             <span>Continue with Google</span>
           </Button>
         </Card>
+
+        {/* Terms and Policy */}
         <p className="text-center text-xs text-muted-foreground">
           By continuing, you agree to Hobbly's{" "}
           <a href="#" className="cursor-pointer text-hobbly-sky-dark">
