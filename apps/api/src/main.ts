206753e45c7ace48b49c7ca41533e10ef49cb2b3
Gvtone <geraldtolentino82@yahoo.com.ph>
2026-05-23 18:34:42 +0800

    Binded 0.0.0.0

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -41,7 +41,7 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api');
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
   Logger.log(`Application is running on: ${await app.getUrl()}`);
 }
 void bootstrap();
