8c22dea47c920761889c6a9e16e0927ccec343c0
Gvtone <geraldtolentino82@yahoo.com.ph>
2026-06-02 14:12:29 +0800

    Fixed initial test

--- apps/api/src/modules/auth/auth.service.spec.ts
+++ apps/api/src/modules/auth/auth.service.spec.ts
@@ -1,12 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UserService } from '../user/user.service';
+import { HashService } from '../../common/utils/hash.service';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { MailService } from '../mail/mail.service';
+import { TokenService } from '../token/token.service';
 
 describe('AuthService', () => {
   let service: AuthService;
 
+  const mockUserService = {};
+  const mockHashService = {};
+  const mockConfigService = {};
+  const mockJwtService = {};
+  const mockMailService = {};
+  const mockTokenService = {};
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        { provide: UserService, useValue: mockUserService },
+        { provide: HashService, useValue: mockHashService },
+        { provide: ConfigService, useValue: mockConfigService },
+        { provide: JwtService, useValue: mockJwtService },
+        { provide: MailService, useValue: mockMailService },
+        { provide: TokenService, useValue: mockTokenService },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
